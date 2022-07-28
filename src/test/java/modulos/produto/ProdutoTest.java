4be5eba29fc0a8f46f7e691466350a62a0419aab
EltonDutradias <elton.dutradias.ti@gmail.com>
2022-07-28 17:05:10 -0300

    Nesta etapa realizamos a automatização: Formulário novo produto, cadastro novo produto e validação de mensagem apresentada.

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -1,5 +1,6 @@
 package modulos.produto;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -9,6 +10,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 @DisplayName("Teste Mobile do Módulo de Produto")
 public class ProdutoTest {
@@ -19,12 +21,13 @@ public class ProdutoTest {
         DesiredCapabilities capacidades = new DesiredCapabilities();
         capacidades.setCapability("deviceName", "Google Pixel 3");
         capacidades.setCapability("platform", "Android");
-        capacidades.setCapability("udid", "192.168.115.102:5555");
+        capacidades.setCapability("udid", "192.168.115.103:5555");
         capacidades.setCapability("appPackage","com.lojinha");
         capacidades.setCapability("appActivity","com.lojinha.ui.MainActivity");
         capacidades.setCapability("app","C:\\Users\\elton\\Desktop\\Lojinha Nativa\\lojinha-nativa.apk");
 
         WebDriver app = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capacidades);
+        app.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         //Fazer login
         app.findElement(By.id("com.lojinha:id/user")).click();
@@ -36,9 +39,24 @@ public class ProdutoTest {
         app.findElement(By.id("com.lojinha:id/loginButton")).click();
 
         //Abrir formulário de novo produto
+        app.findElement(By.id("com.lojinha:id/floatingActionButton")).click();
+
 
         //Cadastrar produto com valor inválido
+        app.findElement(By.id("com.lojinha:id/productName")).click();
+        app.findElement(By.id("com.lojinha:id/productName")).findElement(By.id("com.lojinha:id/editText")).sendKeys("Dell");
+
+        app.findElement(By.id("com.lojinha:id/productValue")).click();
+        app.findElement(By.id("com.lojinha:id/productValue")).findElement(By.id("com.lojinha:id/editText")).sendKeys("700001");
+
+        app.findElement(By.id("com.lojinha:id/productColors")).click();
+        app.findElement(By.id("com.lojinha:id/productColors")).findElement(By.id("com.lojinha:id/editText")).sendKeys("Prata");
+
+        app.findElement(By.id("com.lojinha:id/saveButton")).click();
 
         //Validar que a mensagem de valor inválido foi apresentada
+        String mensagemApresentada = app.findElement(By.xpath("//android.widget.Toast")).getText();
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
+
     }
 }
